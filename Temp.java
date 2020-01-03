255f82bef882cc4bc760fa6a80c0e609c02d27ee
P Vamsi Krishna <vamsikrishna.ponnada@iiitb.org>
2020-01-03 13:53:59 +0530

    Clearing conflicts between master Temp.java and br3 Temp.java

--- Temp.java
+++ Temp.java
@@ -2,6 +2,5 @@ public class Solution {
 	public static void main(String[] arg){ 
 		System.out.println("Hello World");
 		System.out.println("This line is added in the 2nd commit");
-		System.out.println("This line is added by branch br1 after merging to Master branch");
 	}
 }
